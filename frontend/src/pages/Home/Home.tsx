ce4630bbf88f807435fa1d26c9e94a82aca785af
loganyeh <yehlogan@gmail.com>
2026-08-11 11:25:04 -0400

    chore: tailwind, boxicons, react router

---
+++ frontend/src/pages/Home/Home.tsx
@@ -0,0 +1,15 @@
+
+
+function Home(){
+
+    return(
+        <>
+            <div className="border text-red-600">
+                <p>Hello world</p>
+                <i className='bx bx-extension'></i>
+            </div>
+        </>
+    );
+};
+
+export default Home;
