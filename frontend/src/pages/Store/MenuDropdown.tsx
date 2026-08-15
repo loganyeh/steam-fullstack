ad4ebcf674e45754e40d97bc61252407e38dfe5d
loganyeh <yehlogan@gmail.com>
2026-08-15 13:41:39 -0400

    wip: MenuDropdown @ lg breakpoint

--- frontend/src/pages/Store/MenuDropdown.tsx
+++ frontend/src/pages/Store/MenuDropdown.tsx
@@ -1,17 +1,28 @@
 import SearchTheStore from "../../components/SearchTheStore";
 
 function MenuDropdown(){
+    const lgMenu = [
+        "Browse", "Recommendations", "Categorites", "Hardware", "More"
+    ]
     
     return(
         <>
             <section className="border border-red-600 fixed z-10 flex justify-between px-2 py-3 w-full bg-gray-200">
                 {/* menu dropdown */}
-                <div className="flex items-center">
+                <div className="lg:hidden flex items-center">
                     <p className="font-bold text-sm">Menu</p>
                     <i className='bx bx-chevron-down text-lg'></i>
                 </div>
 
-                <SearchTheStore className="hidden md:flex absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:max-w-lg" />
+                {lgMenu.map((tab, index) => {
+                    return <div key={index} className="hidden md:flex items-center">
+                    <p className="font-bold text-sm lg:text-xs">{tab}</p>
+                    <i className='bx bx-chevron-down text-lg'></i>
+                </div>
+                })}
+
+                {/* <SearchTheStore className="hidden md:flex lg:static lg:translate-0 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:max-w-lg lg:max-w-sm" /> */}
+                {/* fix padding in section parent when search comp is enabled vs disabled */}
 
                 {/* wishlist */}
                 <div className="flex gap-1 items-center">
